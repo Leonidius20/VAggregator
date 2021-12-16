9d22499757ce5b0ea2fd9239f3755d0f1f87ce10
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2021-12-16 18:58:45 +0200

    implemented library

--- app/src/main/java/io/github/leonidius20/vaggregator/ui/library/LibraryFragment.kt
+++ app/src/main/java/io/github/leonidius20/vaggregator/ui/library/LibraryFragment.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navGraphViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import io.github.leonidius20.vaggregator.R
 import io.github.leonidius20.vaggregator.databinding.FragmentLibraryBinding
+import io.github.leonidius20.vaggregator.ui.movie_details.SelectedContentSharedViewModel
+import io.github.leonidius20.vaggregator.ui.movies.search_results_list.SearchResultsAdapter
 
 class LibraryFragment : Fragment() {
 
-    private lateinit var libraryViewModel: LibraryViewModel
+    private val libraryViewModel: LibraryViewModel by navGraphViewModels(R.id.mobile_navigation)
     private var _binding: FragmentLibraryBinding? = null
+
+    private val selectedMovieViewModel: SelectedContentSharedViewModel by activityViewModels()
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,17 +30,32 @@ class LibraryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        libraryViewModel =
-            ViewModelProvider(this).get(LibraryViewModel::class.java)
-
         _binding = FragmentLibraryBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        libraryViewModel.text.observe(viewLifecycleOwner, {
-            textView.text = it
-        })
-        return root
+        binding.libraryContentRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        libraryViewModel.data.observe(viewLifecycleOwner) {
+            if (it.isEmpty()) {
+                binding.libraryNoSavedContent.visibility = View.VISIBLE
+                binding.libraryContentRecyclerView.visibility = View.GONE
+            } else {
+                binding.libraryNoSavedContent.visibility = View.GONE
+
+                binding.libraryContentRecyclerView.apply {
+                    visibility = View.VISIBLE
+                    adapter =
+                        SearchResultsAdapter(it!!.toTypedArray()) { selectedMovie ->
+                            selectedMovieViewModel.select(selectedMovie)
+                            val action = LibraryFragmentDirections.actionLibraryToContentDetails()
+                            findNavController().navigate(action)
+                        }
+                }
+            }
+        }
+
+        libraryViewModel.loadData()
+
+        return binding.root
     }
 
     override fun onDestroyView() {
--- app/src/main/java/io/github/leonidius20/vaggregator/ui/library/LibraryViewModel.kt
+++ app/src/main/java/io/github/leonidius20/vaggregator/ui/library/LibraryViewModel.kt
@@ -1,16 +1,21 @@
 package io.github.leonidius20.vaggregator.ui.library
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import io.github.leonidius20.vaggregator.data.PieceOfContent
 import io.github.leonidius20.vaggregator.data.library.LibraryRepository
+import kotlinx.coroutines.launch
 
 class LibraryViewModel : ViewModel() {
 
     private val repository = LibraryRepository.instance
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+    val data = MutableLiveData<List<PieceOfContent>>()
+
+    fun loadData() {
+        viewModelScope.launch {
+            data.value = repository.getAll()
+        }
     }
-    val text: LiveData<String> = _text
 }
