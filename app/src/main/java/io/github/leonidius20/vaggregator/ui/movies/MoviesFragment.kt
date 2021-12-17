5b292caf39a6c2c27366d88095b9ccc99dd0bfb8
Leonidius20 <25536411+Leonidius20@users.noreply.github.com>
2021-12-17 20:19:11 +0200

    refactored movie fragment

--- app/src/main/java/io/github/leonidius20/vaggregator/ui/movies/MoviesFragment.kt
+++ app/src/main/java/io/github/leonidius20/vaggregator/ui/movies/MoviesFragment.kt
@@ -4,26 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.NavDirections
 import androidx.navigation.navGraphViewModels
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.android.material.snackbar.Snackbar
-import com.mancj.materialsearchbar.MaterialSearchBar
 import io.github.leonidius20.vaggregator.R
-import io.github.leonidius20.vaggregator.data.Status
 import io.github.leonidius20.vaggregator.databinding.FragmentMoviesBinding
-import io.github.leonidius20.vaggregator.ui.base.list.SearchResultsAdapter
-import io.github.leonidius20.vaggregator.ui.hideKeyboard
-import io.github.leonidius20.vaggregator.ui.isNetworkConnected
-import io.github.leonidius20.vaggregator.ui.movie_details.SelectedContentSharedViewModel
+import io.github.leonidius20.vaggregator.ui.base.search.BaseSearchFragment
 
-class MoviesFragment : Fragment(), MaterialSearchBar.OnSearchActionListener {
-
-    private val moviesViewModel: MoviesViewModel by navGraphViewModels(R.id.mobile_navigation)
-
-    private val selectedMovieViewModel: SelectedContentSharedViewModel by activityViewModels()
+class MoviesFragment : BaseSearchFragment<MoviesViewModel>() {
 
     private var _binding: FragmentMoviesBinding? = null
 
@@ -31,59 +18,9 @@ class MoviesFragment : Fragment(), MaterialSearchBar.OnSearchActionListener {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
+    override fun setupView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentMoviesBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        binding.moviesSearchResultsRecyclerView.apply {
-            layoutManager = LinearLayoutManager(context)
-        }
-
-        moviesViewModel.movies.observe(viewLifecycleOwner) {
-            when (it.status) {
-                Status.SUCCESS -> {
-                    binding.moviesProgress.visibility = View.GONE
-
-                    binding.moviesSearchResultsRecyclerView.apply {
-                        visibility = View.VISIBLE
-                        adapter =
-                            SearchResultsAdapter(it.data!!.toTypedArray()) { selectedMovie ->
-                                selectedMovieViewModel.select(selectedMovie)
-                                val action = MoviesFragmentDirections
-                                    .actionMoviesToMovieDetails()
-                                findNavController().navigate(action)
-                            }
-                    }
-
-                    if (it.message != null && !moviesViewModel.errorShown.value!!) {
-                        Snackbar.make(activity!!.window.decorView,
-                            it.message.toString(), Snackbar.LENGTH_LONG).show()
-                        moviesViewModel.errorShown.value = true
-                    }
-
-                    binding.moviesSearchNoResults.visibility = if (it.data!!.isEmpty()) View.VISIBLE else View.GONE
-                }
-                Status.ERROR -> {
-                    binding.moviesProgress.visibility = View.GONE
-                    binding.moviesSearchNoResults.visibility = View.GONE
-                    binding.moviesSearchResultsRecyclerView.visibility = View.GONE
-                    Snackbar.make(activity!!.window.decorView,  it.message.toString(), Snackbar.LENGTH_LONG).show()
-                }
-                else -> {
-                    binding.moviesProgress.visibility = View.VISIBLE
-                    binding.moviesSearchNoResults.visibility = View.GONE
-                    binding.moviesSearchResultsRecyclerView.visibility = View.GONE
-                }
-            }
-        }
-
-        binding.moviesSearchBar.setOnSearchActionListener(this)
-
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
@@ -91,22 +28,12 @@ class MoviesFragment : Fragment(), MaterialSearchBar.OnSearchActionListener {
         _binding = null
     }
 
-    override fun onSearchStateChanged(enabled: Boolean) {
-
+    override fun createViewModel(): Lazy<MoviesViewModel> {
+        return navGraphViewModels(R.id.mobile_navigation)
     }
 
-    override fun onSearchConfirmed(text: CharSequence?) {
-        if (text.isNullOrBlank()) return
-        hideKeyboard()
-        if (!isNetworkConnected()) {
-            Snackbar.make(activity!!.window.decorView, R.string.no_internet, Snackbar.LENGTH_LONG).show()
-        } else {
-            moviesViewModel.loadMovies(text.toString())
-        }
-    }
-
-    override fun onButtonClicked(buttonCode: Int) {
-
+    override fun createToContentActionDetails(): NavDirections {
+        return MoviesFragmentDirections.actionMoviesToMovieDetails()
     }
 
 }
--- app/src/main/java/io/github/leonidius20/vaggregator/ui/movies/MoviesViewModel.kt
+++ app/src/main/java/io/github/leonidius20/vaggregator/ui/movies/MoviesViewModel.kt
@@ -1,31 +1,16 @@
 package io.github.leonidius20.vaggregator.ui.movies
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import io.github.leonidius20.vaggregator.data.PieceOfContent
 import io.github.leonidius20.vaggregator.data.Resource
-import io.github.leonidius20.vaggregator.data.movies.Movie
 import io.github.leonidius20.vaggregator.data.movies.MoviesRepository
-import kotlinx.coroutines.launch
+import io.github.leonidius20.vaggregator.ui.base.search.BaseSearchViewModel
 
-class MoviesViewModel : ViewModel() {
+class MoviesViewModel : BaseSearchViewModel() {
 
     private val repository = MoviesRepository()
 
-    val movies = MutableLiveData<Resource<List<Movie>>>()
-
-    val errorShown = MutableLiveData(false) // temp code to not re-show error messages
-
-    fun loadMovies(q: String) {
-        movies.value = Resource.loading(null)
-        errorShown.value = false
-        viewModelScope.launch {
-            try {
-                movies.value = repository.findMoves(q)
-            } catch (e: Exception) {
-                movies.value = Resource.error(e.message!!, null)
-            }
-        }
+    override suspend fun obtainData(): Resource<List<PieceOfContent>> {
+        return repository.findMoves(searchQuery)
     }
 
 }
